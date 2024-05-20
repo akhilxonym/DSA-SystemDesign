dba5f663f1f7fc0a03b8ac494b44811e9a843077
Akhil Nigam <akhilnigam96@gmail.com>
2024-05-20 14:49:50 +0530

    finished client calling fixedWindowRL

--- System-Design/Rate-Limiter/src/App.java
+++ System-Design/Rate-Limiter/src/App.java
@@ -2,7 +2,16 @@ package src;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        RateLimiterStrategy fixedWindowRLStrategy = new FixedWindowRateLimiter(5, 10);
+        RateLimiter rateLimiter = new RateLimiter(fixedWindowRLStrategy);
+
+        for (int i = 0; i < 10; i++) {
+            if (rateLimiter.allowRequest("user101")) {
+                System.out.println("Request " + (i + 1) + " allowed");
+            } else {
+                System.out.println("Request " + (i + 1) + " not allowed");
+            }
+        }
     }
 
     public String test() {
